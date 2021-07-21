c3bceaa5a890e7745ee1fe5b1a681bea00512f79
Kawaggy <emorei.game@gmail.com>
2021-07-21 20:26:55 +0100

    Multiplayer fix for Junko's Aura

--- JunkoAndFriends.cs
+++ JunkoAndFriends.cs
@@ -25,8 +25,9 @@ namespace JunkoAndFriends
                 AddEquipTexture(null, EquipType.Legs, "AmeliaLeg_Legs", "JunkoAndFriends/Items/AmeliaVanity/AmeliaLeg_Legs");
                 GameShaders.Armor.BindShader(ModContent.ItemType<WaveShaderDye>(), new ArmorShaderData(new Ref<Effect>(GetEffect("Effects/WaveShader")), "WaveShaderPass"));
                 GameShaders.Misc["WaveShader"] = new MiscShaderData(new Ref<Effect>(GetEffect("Effects/WaveShader")), "WaveShaderPass");
+                JunkoAndFriendsRenderTargets.Initialize();
             }
-            JunkoAndFriendsRenderTargets.Initialize();
+            
             On.Terraria.Main.Draw += Main_Draw;
 
             SpecialEffectKey = RegisterHotKey("Special Vanity Effect", "NumPad5");
@@ -57,7 +58,8 @@ namespace JunkoAndFriends
 
         private void Main_Draw(On.Terraria.Main.orig_Draw orig, Main self, Microsoft.Xna.Framework.GameTime gameTime)
         {
-            JunkoAndFriendsRenderTargets.Draw();
+            if (!Main.dedServ)
+                JunkoAndFriendsRenderTargets.Draw();
             orig(self, gameTime);
         }
 
